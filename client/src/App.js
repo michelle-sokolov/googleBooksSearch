3fc68bc94b5e6a42ea612ad4ebf27f280a047a74
Michelle Sokolov <michellesokolov@Michelles-MacBook-Pro.local>
2020-03-10 15:59:47 -0700

    removed page & switched route paths

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
@@ -11,10 +10,9 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Books} />
+          <Route exact path="/" component={Detail} />
           <Route exact path="/books" component={Books} />
           <Route exact path="/search/" component={Detail} />
-          <Route component={NoMatch} />
         </Switch>
       </div>
     </Router>
